ff13142f1a9fa01e37b2adb5af69d195d9ecf08b
Roman lesyo <ladiginscormag@gmail.com>
2023-12-19 18:04:23 +0200

    commit src/components/Header/Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import { Title } from "@mantine/core";
 import * as s from "./Header.css";
-import { ColorSchemeToggle } from './components/ColorSchemeToggle/ColorSchemeToggle';
-
+import { ColorSchemeToggle } from './ui/components/ColorSchemeToggle/ColorSchemeToggle';
 export function Header() {
 	return (
 		<section className={s.wrapper} >
+			<Title className={s.title} ta="center" mt={100}>
+				header
+			</Title>
 			<Title className={s.title} ta="center" mt={100}>
 				header
 			</Title>
